7bbe076a1c4767851dcf3af20a8c8781206934ce
Alexey Karpov <you@example.com>
2017-04-10 15:39:15 +0300

    added collection

--- L2.1/src/main/java/ru/otus/Main.java
+++ L2.1/src/main/java/ru/otus/Main.java
@@ -5,6 +5,7 @@ import ru.otus.factory.NewObjectFactory;
 import ru.otus.factory.ObjectFactory;
 
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
 
 /**
  * Created by Alexey on 01.04.2017.
@@ -24,6 +25,31 @@ class StringFactory implements ObjectFactory {
     }
 }
 
+class StringArrayListFactory implements ObjectFactory {
+    int count = 0;
+
+
+    StringArrayListFactory(int count)
+    {
+        this.count = count;
+    }
+    @Override
+    public Object createObject() {
+        ArrayList<String> arrayList = new ArrayList<>(count);
+        for (int i = 0; i < count; ++i)
+        {
+            arrayList.add(new String());
+        }
+
+        return arrayList;
+    }
+
+    @Override
+    public Class<?> getObjectClass() {
+        return ArrayList.class;
+    }
+}
+
 
 public class Main  {
 
@@ -39,16 +65,27 @@ public class Main  {
             ObjectSize.calculatecalculateByMemoryAllocation(className, objectCount, 1);
         }
 
-        System.out.println("=== Example 2. Empty class String ===");
+        System.out.println("\n=== Example 2. Empty class String ===");
         ObjectSize.calculatecalculateByMemoryAllocation(String.class, objectCount, 3);
 
-        System.out.println("=== Example 3. My test class ===");
+        System.out.println("\n=== Example 3. My test class ===");
         ObjectSize.calculatecalculateByMemoryAllocation(new TestFactory(), objectCount, 1);
 
-        System.out.println("=== Example 4. String class with text \"Hello world\" ===");
+        System.out.println("\n=== Example 4. String class with text \"Hello world\" ===");
         ObjectSize.calculatecalculateByMemoryAllocation(new StringFactory(), objectCount, 1);
 
-        System.out.println("=== Example 5. Dynamic create string class with text \"Hello world\" ===");
+        System.out.println("\n=== Example 5. Dynamic create string class with text \"Hello world\" ===");
         ObjectSize.calculatecalculateByMemoryAllocation(new NewObjectFactory(String.class, new Class<?>[]{String.class}, new String[]{"Hello world"}), objectCount, 1);
+
+        // Опеределяем зависимость размера объекта коллекции от количества элементов
+        // Стараемся во всех экспериментах примерно выделять одинаковое количество памяти,
+        // поэтому количество созданных коллекций зависит от числа элементов в них
+        System.out.println("\n=== Example 6. Arraylist example (empty) ===" + "string in collection = 0");
+        ObjectSize.calculatecalculateByMemoryAllocation(new StringArrayListFactory(0), objectCount, 1);
+
+        for (int i = 1; i < objectCount; i *= 10) {
+            System.out.println("\n=== Example 6. Arraylist example (empty) ===" + "string in collection = " + i);
+            ObjectSize.calculatecalculateByMemoryAllocation(new StringArrayListFactory(i), objectCount / i, 1);
+        }
     }
 }
